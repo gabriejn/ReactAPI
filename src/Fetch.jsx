46b645fc891600bf9b684798f82bfd1ddb35957c
Gabriel Navarro <gabriejn@uci.edu>
2024-04-09 14:51:16 -0700

    Updated React API

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -1,37 +1,66 @@
-
 import { useState, useEffect } from 'react';
-//const accessKey="-LNZKxd36AmzQRJr3qnqpxg1DsM4srEp8Bpsc7-PO4k"
-const accessKey="d3Qk6wq33VuRu5syhl8_Hd2Xr4vbm5Wy-9Nm3gk4eFs"
-//const accessKey="ursiD7vOoapHZWKR_E2g7tlq_BecYHXOzBPtlJlVhV8"
+import './App.css'
 const apiUrl = 'https://api.unsplash.com/photos/random';
 let displayURL=[]
-let photos = []
+let displayURL1=[]
+let textcontent= []
+let textcontent1= []
+let accessKey =prompt('enter your api key');
+
 function Fetch() {
  const [photos, setPhotos] = useState();
   useEffect(() => {
     fetch(`${apiUrl}?client_id=${accessKey}`)
       .then((res) => {
         return res.json();
-      })
-      .then((data) => {
 
-        console.log(data.urls.regular)
+      })
+      .then((data) => { 
+
+        textcontent=data.alt_description
+         textcontent = textcontent.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+
 
         displayURL=(data.urls.regular);
         setPhotos(data);
-        photos(data)
       });
 
  }, []);
-       console.log(displayURL.values)
+ useEffect(() => {
+  fetch(`${apiUrl}?client_id=${accessKey}`)
+    .then((res) => {
+      return res.json();
+
+    })
+    .then((data) => { 
+
+      console.log(data.urls.regular)
+      
+      displayURL1=(data.urls.regular);
+      textcontent1=data.alt_description
+      textcontent1 = textcontent1.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+
+      console.log(data.location)
+
+      setPhotos(data);
+    });
+
+}, []);
 
       return  (
-<div>
-             <h1>Hekko Workd</h1>
-             <img src={displayURL} />
-
-                     console.log(data.urls.regular)
-
+<div >
+             <h1>Gabriel N. Unsplash API Project</h1>
+             <div className='divbox'>
+              <div className='firstimage'>
+             <img id= 'imgdisplay' style={{maxHeight:'55vh'}} src={displayURL1} />
+             <br></br>
+             <h2>{textcontent1}</h2>
+              </div>
+             <div className='secondimage'>
+             <img id= 'imgdisplay'style={{maxHeight:'55vh'}} src={displayURL} />
+             <h2>{textcontent}</h2>
+              </div>
+</div>
   </div>)
  
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
+import('dotenv').config
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
 import Fetch from './Fetch.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
     <Fetch />
   </React.StrictMode>,
 )
